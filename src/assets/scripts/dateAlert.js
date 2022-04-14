3924d5a97a4a24060ccf4c2a1f59a978a540f18d
Глеб <gleblubiskin@gmail.com>
2022-04-14 21:45:48 +0300

    Development and implementation of the current time counter updated every second

---
+++ src/assets/scripts/dateAlert.js
@@ -0,0 +1,15 @@
+// Html element
+const dateAlert = document.getElementById("date-alert");
+
+// Function to get date now
+const getTime = () => {
+  return new Date().toTimeString().replace(/ .*/, '');
+}
+
+// For instant display on the page, without waiting for setInterval
+dateAlert.innerHTML = getTime();
+
+// Async refresh
+setInterval(() => {
+  dateAlert.innerHTML = getTime();
+}, 1000)
